7ea4068b2654552d1df77711cfdd371745e22ac6
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2021-10-19 17:51:02 -0300

    Correção requisito 02

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,73 @@
 import React from 'react';
+import { Redirect } from 'react-router';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.onChange = this.onChange.bind(this);
+    this.onClick = this.onClick.bind(this);
+
+    this.state = {
+      user: '',
+      buttonDisabled: true,
+      isLoading: false,
+      logged: false,
+    };
+  }
+
+  onChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const minLength = 3;
+
+    this.setState({
+      [name]: value,
+    });
+
+    if (value.length >= minLength) {
+      this.setState({
+        buttonDisabled: false,
+      });
+    }
+  }
+
+  onClick() {
+    const { user } = this.state;
+    this.setState({
+      isLoading: true,
+    }, async () => {
+      await createUser({ name: user });
+      this.setState({
+        isLoading: false,
+        logged: true,
+      });
+    });
+  }
+
   render() {
+    const { user, buttonDisabled, isLoading, logged } = this.state;
     return (
       <div data-testid="page-login">
-        <p>Login</p>
+        <input
+          type="text"
+          name="user"
+          id="login"
+          data-testid="login-name-input"
+          onChange={ this.onChange }
+          value={ user }
+        />
+        <button
+          type="button"
+          disabled={ buttonDisabled }
+          onClick={ this.onClick }
+          data-testid="login-submit-button"
+        >
+          Entrar
+        </button>
+        { isLoading === true && <Loading /> }
+        { logged === true && <Redirect to="/search" /> }
       </div>
     );
   }
