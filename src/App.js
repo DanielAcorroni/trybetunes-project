84d80fa7aa645f33cfbd469936a4b4b87b9e3c0c
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2021-10-19 15:48:48 -0300

    Realiza requisito 01

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
 import React from 'react';
+import { Route, Switch } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <>
+        <p>TrybeTunes</p>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/">
+              <Login />
+            </Route>
+            <Route path="/search">
+              <Search />
+            </Route>
+            <Route path="/album" render={ (props) => <Album { ...props } /> } />
+            <Route path="/favorites">
+              <Favorites />
+            </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+            <Route
+              path="/profile/edit"
+              render={ (props) => <ProfileEdit { ...props } /> }
+            />
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </>
+    );
   }
 }
 
