40780810e2a5577de4a562f716317ec7419e79a1
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2021-10-20 16:03:30 -0300

    Realiza requisito 04

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Loading from '../pages/Loading';
 import { getUser } from '../services/userAPI';
 
@@ -46,6 +47,9 @@ class Header extends React.Component {
     return (
       <header data-testid="header-component">
         <h1>Header</h1>
+        <Link data-testid="link-to-search" to="/search">Search</Link>
+        <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
+        <Link data-testid="link-to-profile" to="/profile">Profile</Link>
         <p data-testid="header-user-name">{userName}</p>
         {isLoading === true && <Loading />}
       </header>
