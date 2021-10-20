6edd24d8ce7bc4df11810e7b542e8da9ac90fdf7
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2021-10-20 18:42:38 -0300

    Realiza requisito 06

---
+++ src/components/Cards.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+class Cards extends React.Component {
+  render() {
+    const { album } = this.props;
+    const {
+      artistName,
+      collectionId,
+      collectionName,
+      artworkUrl100,
+    } = album;
+    console.log(album);
+    const path = `/album/${collectionId}`;
+
+    return (
+      <Link to={ path } data-testid={ `link-to-album-${collectionId}` }>
+        <div>
+          <img src={ artworkUrl100 } alt="Imagem do álbum" />
+          <p>{collectionName}</p>
+          <p>{artistName}</p>
+        </div>
+      </Link>
+    );
+  }
+}
+
+Cards.propTypes = {
+  album: PropTypes.shape({
+    artistName: PropTypes.string.isRequired,
+    collectionId: PropTypes.number.isRequired,
+    collectionName: PropTypes.string.isRequired,
+    artworkUrl100: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default Cards;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import Cards from '../components/Cards';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from './Loading';
 
 class Search extends React.Component {
   constructor() {
     super();
     this.onChange = this.onChange.bind(this);
+    this.onClick = this.onClick.bind(this);
 
     this.state = {
       artist: '',
+      prevSearch: '',
       buttonDisabled: true,
+      isLoading: false,
+      albunsInterface: [],
+      loaded: false,
     };
   }
 
@@ -32,8 +40,39 @@ class Search extends React.Component {
     }
   }
 
+  onClick() {
+    const { artist } = this.state;
+    this.setState({
+      isLoading: true,
+    });
+    searchAlbumsAPI(artist).then((response) => {
+      this.setState({
+        isLoading: false,
+        albunsInterface: response,
+        prevSearch: artist,
+        artist: '',
+        loaded: true,
+      });
+    });
+  }
+
   render() {
-    const { artist, buttonDisabled } = this.state;
+    const {
+      artist,
+      buttonDisabled,
+      isLoading,
+      albunsInterface,
+      loaded,
+      prevSearch } = this.state;
+    const resultText = `Resultado de álbuns de: ${prevSearch}`;
+    if (isLoading === true) {
+      return (
+        <>
+          <Header />
+          <Loading />
+        </>
+      );
+    }
     return (
       <div data-testid="page-search">
         <Header />
@@ -48,9 +87,17 @@ class Search extends React.Component {
           data-testid="search-artist-button"
           type="button"
           disabled={ buttonDisabled }
+          onClick={ this.onClick }
         >
           Procurar
         </button>
+        {loaded === true && <p>{resultText}</p>}
+        {
+          albunsInterface.length > 0 ? albunsInterface.map((singAlbum) => (<Cards
+            key={ singAlbum.collectionId }
+            album={ singAlbum }
+          />)) : <p>Nenhum álbum foi encontrado</p>
+        }
       </div>
     );
   }
